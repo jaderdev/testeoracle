00855c833d4e1efd7490ac1a485a919a431095ec
Jaderson Nascimento <daibushi135@gmail.com>
2019-08-19 14:58:00 -0300

    Adicionando index de tasks

--- db/migrate/20190819172030_create_tasks.rb
+++ db/migrate/20190819172030_create_tasks.rb
@@ -1,7 +1,8 @@
 class CreateTasks < ActiveRecord::Migration[5.2]
   def change
     create_table :tasks do |t|
-
+      t.string :descricao
+      t.integer :status
       t.timestamps
     end
   end
