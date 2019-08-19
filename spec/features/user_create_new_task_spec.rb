8a33122faca670dfca2fe2c8726e87d34e6ce7a3
Jaderson Nascimento <daibushi135@gmail.com>
2019-08-19 17:22:04 -0300

    Adicionando select a criação do status

--- spec/features/user_create_new_task_spec.rb
+++ spec/features/user_create_new_task_spec.rb
@@ -5,6 +5,7 @@ feature 'User create a new task' do
 		task = Task.new(descricao: 'Uma tarefa simples para testar', status: 0)
 		visit new_task_url
 		fill_in 'Descrição da tarefa',with: task.descricao
+		select 'TODO', from: 'Status da tarefa'
 		click_on 'Criar'
 
 		expect(page).to have_css('td',text: task.descricao)
