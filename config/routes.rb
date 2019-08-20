dab4fa18016889d2ed6f2a25d89b538f1bc5ca67
Jaderson Nascimento <daibushi135@gmail.com>
2019-08-19 22:31:59 -0300

    Estilização das páginas

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-
+  root to: 'task#index'
   resources :task
   get '/task/filtered', action: :filter, controller: 'tasks', as: 'task_filter'
   get '/task/toggle/:id', action: :toggle_status, controller: 'task', as: 'toggle_status'
